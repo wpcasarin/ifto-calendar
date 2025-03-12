361b9aab9b2e769a0433f0686720ea90aead5c1a
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2025-03-12 00:23:33 -0300

    update readme

---
+++ app/README.md
@@ -0,0 +1,8 @@
+# IFTO Calendar
+
+Calendário oficial dos cursos superiores do IFTO.
+
+- Feito para uso pessoal.
+- Possui a funcionalidade de adicionar eventos ao Google Agenda.
+
+[Acesse aqui](https://ifto-calendar.netlify.app/)
--- app/src/components/Calendar.tsx
+++ app/src/components/Calendar.tsx
@@ -159,9 +159,9 @@ export const Calendar = () => {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="size-4"
                           >
                             <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
